87114832763f139e3afadb434280b5ec7e853220
Benjamin Hall <b.e.s.hall@outlook.com>
2018-06-20 11:08:33 +0100

    Initial Commit

---
+++ Coordinate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinate
+//
+//  Created by Developer on 20/06/2018.
+//  Copyright © 2018 beshbashbosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
